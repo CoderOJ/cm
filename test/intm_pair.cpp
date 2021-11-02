8288acfa5bebf016d9c2f662030398600cc9f92e
CoderOJ <CoderOJ@outlook.com>
2021-11-03 07:10:25 +0800

    feat(intm_pair): add integer under multiple mods

---
+++ test/intm_pair.cpp
@@ -0,0 +1,17 @@
+#include "../intm_pair.hpp"
+#include "../debug.hpp"
+#include "../intm"
+
+int main()
+{
+  using int_ta = cm::intm<11>;
+  using int_tb = cm::intm<103>;
+  using int_t = cm::intm_pair<int_ta, int_tb>;
+
+  see(int_t(1));
+  see(int_t(27));
+  see(int_t(13) + int_t(27));
+  see(int_t(13) - int_t(27));
+  see(int_t(13) * int_t(27));
+  see(int_t(13) / int_t(27));
+}
