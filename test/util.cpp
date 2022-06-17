463b335d64de8aa7227f5796a72a77b5fec478b7
CoderOJ <CoderOJ@outlook.com>
2022-06-17 14:54:35 +0800

    upd(util): remove operators for std::pair

--- test/util.cpp
+++ test/util.cpp
@@ -1,9 +1,6 @@
 #include "../util.hpp"
 #include "../debug"
 
-AT_INIT({ see("before main"); });
-AT_EXIT({ see("after main"); });
-
 int main()
 {
   {
@@ -15,18 +12,11 @@ int main()
     arr[0] = 1;
   }
 
-  AT_INIT({ see("hello"); });
-  AT_EXIT({ see("good bye"); });
-
-  std::pair<int, int> a(1, 2), b(11, 12);
-  cm_assert(a + b == std::make_pair(1 + 11, 2 + 12));
-  cm_assert(a - b == std::make_pair(1 - 11, 2 - 12));
-
   cm::once_t once;
   cm_assert(once());
   cm_assert(!once());
 
-  auto fac = y_combinate(
+  auto fac = cm::y_combinate(
       [](auto f, int x) -> int { return x == 0 ? 1 : x * f(x - 1); });
   cm_assert(fac(5) == 5 * 4 * 3 * 2 * 1);
 }
--- util.hpp
+++ util.hpp
@@ -5,8 +5,6 @@
 #include <utility>
 #include <vector>
 
-#define _INLINE inline __attribute__((always_inline))
-
 namespace cm
 {
 
@@ -55,63 +53,6 @@ using array = typename impl::__array<T, n0...>::type;
 using cm::check_max;
 using cm::check_min;
 
-template <class A, class B>
-_INLINE std::pair<A, B> operator+(const std::pair<A, B> &lhs,
-                                  const std::pair<A, B> &rhs)
-{
-  return std::make_pair(lhs.first + rhs.first, lhs.second + rhs.second);
-}
-template <class A, class B>
-_INLINE std::pair<A, B> operator-(const std::pair<A, B> &lhs,
-                                  const std::pair<A, B> &rhs)
-{
-  return std::make_pair(lhs.first - rhs.first, lhs.second - rhs.second);
-}
-template <class A, class B>
-_INLINE std::pair<A, B> &operator+=(std::pair<A, B>       &lhs,
-                                    const std::pair<A, B> &rhs)
-{
-  lhs.first += rhs.first;
-  lhs.second += rhs.second;
-  return lhs;
-}
-template <class A, class B>
-_INLINE std::pair<A, B> &operator-=(std::pair<A, B>       &lhs,
-                                    const std::pair<A, B> &rhs)
-{
-  lhs.first -= rhs.first;
-  lhs.second -= rhs.second;
-  return lhs;
-}
-template <class A, class B>
-_INLINE std::pair<A, B> operator*(const std::pair<A, B> &lhs,
-                                  const std::pair<A, B> &rhs)
-{
-  return std::make_pair(lhs.first * rhs.first, lhs.second * rhs.second);
-}
-template <class A, class B>
-_INLINE std::pair<A, B> operator/(const std::pair<A, B> &lhs,
-                                  const std::pair<A, B> &rhs)
-{
-  return std::make_pair(lhs.first / rhs.first, lhs.second / rhs.second);
-}
-template <class A, class B>
-_INLINE std::pair<A, B> &operator*=(std::pair<A, B>       &lhs,
-                                    const std::pair<A, B> &rhs)
-{
-  lhs.first *= rhs.first;
-  lhs.second *= rhs.second;
-  return lhs;
-}
-template <class A, class B>
-_INLINE std::pair<A, B> &operator/=(std::pair<A, B>       &lhs,
-                                    const std::pair<A, B> &rhs)
-{
-  lhs.first /= rhs.first;
-  lhs.second /= rhs.second;
-  return lhs;
-}
-
 #if __cplusplus >= 201402L
 namespace cm
 {
@@ -137,24 +78,10 @@ y_combinate_t<std::decay_t<F>> y_combinate(F &&f)
 } // namespace cm
 #endif
 
-// clang-format off
-#define M__AT_INIT(line, Pred) struct CM_INNER_ATINIT##line##_t { \
-  CM_INNER_ATINIT##line## _t () Pred } CM_INNER_ATINIT##line
-#define M_AT_INIT(line, Pred) M__AT_INIT(line, Pred)
-#define AT_INIT(Pred) M_AT_INIT(__LINE__, Pred)
-
-#define M__AT_EXIT(line, Pred) struct CM_INNER_ATEXIT##line##_t { \
-  ~CM_INNER_ATEXIT##line## _t () Pred } CM_INNER_ATEXIT##line
-#define M_AT_EXIT(line, Pred) M__AT_EXIT(line, Pred)
-#define AT_EXIT(Pred) M_AT_EXIT(__LINE__, Pred)
-// clang-format on
-
 #ifdef CM_DEBUG
 #define CONSTRAINT(n, a, b) constexpr auto n = b;
 #else
 #define CONSTRAINT(n, a, b) constexpr auto n = a;
 #endif
-
-#undef _INLINE
 
 #endif
