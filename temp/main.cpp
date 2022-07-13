b57f6ad49b5f60469d3c751099ba772c6b812bf0
CoderOJ <CoderOJ@outlook.com>
2022-07-13 20:07:49 +0800

    feat(temp/main.cpp): add header "math_util"

--- temp/main.cpp
+++ temp/main.cpp
@@ -1,6 +1,7 @@
 #include "/home/jack/cm/0pch.hpp"
 #include "/home/jack/cm/debug.hpp"
 // #include "/home/jack/cm/intm.hpp"
+// #include "/home/jack/cm/math_util.hpp"
 #include "/home/jack/cm/scanner.hpp"
 // #include "/home/jack/cm/segment_tree.hpp"
 // #include "/home/jack/cm/string.hpp"
